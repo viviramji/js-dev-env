4631b609fb900fe57952ebc93d1386e4b1a75d82
Victor Victor RJ <wrjr04@gmail.com>
2023-08-21 15:46:35 -0500

    Add test files.

---
+++ src/index.test.js
@@ -0,0 +1,20 @@
+import { expect } from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs';
+
+describe('Our frist test', () => {
+  it('Should pass', () => {
+    expect(true).to.equal(true);
+  });
+});
+
+describe('Checking index.html', () => {
+  it('Should say hello world', () => {
+    const index = fs.readFileSync('./src/index.html', 'utf-8');
+    const {JSDOM } = jsdom;
+    const dom = new JSDOM(index);
+    const h1 = dom.window.document.getElementsByTagName('h1')[0];
+    expect(h1.innerHTML).to.equal('Hello World!');
+    dom.window.close();
+  });
+});
