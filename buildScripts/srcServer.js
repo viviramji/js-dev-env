e1b5c1104f2bbbd309b678132861bb50ddf120d3
Victor Victor RJ <wrjr04@gmail.com>
2023-08-21 12:39:04 -0500

    Add babel config and change file ES6.

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,8 +1,8 @@
-const express  = require('express');
-const path = require('path');
-const open = require('open');;
+import express from 'express';
+import path from 'path';
+import open from 'open';
 
-let port = 3000;
+const port = 3000;
 const app = express();
 
 app.get('/', function (req, res) {
