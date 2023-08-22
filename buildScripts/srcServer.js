a86520f88d6ded99fab69f1eca141a51314924b5
Victor Victor RJ <wrjr04@gmail.com>
2023-08-21 21:29:29 -0500

    Add get http request

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -18,6 +18,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function (req, res) {
+  res.json([
+    { "id": 1, "firstName": "Bob", "lastName": "Smith", "email": "test@null.com"},
+    { "id": 2, "firstName": "Tammy", "lastName": "Norton", "email": "test2@null.com"},
+    { "id": 3, "firstName": "Tina", "lastName": "Lee", "email": "test2@null.com"},
+  ]);
+});
+
 app.listen(port, function (error) {
   if (error) {
     console.log(error);
