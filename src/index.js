4d62097808582e8cde2ed742715702ec084c8aca
Victor Victor RJ <wrjr04@gmail.com>
2023-08-21 22:24:28 -0500

    Add delete logic into index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './index.css';
-import getUsers from './api/userApi';
+import { getUsers, deleteUser } from './api/userApi';
 
 getUsers().then(result => {
   let usersBody = "";
@@ -15,4 +15,19 @@ getUsers().then(result => {
   });
 
   global.document.getElementById('users').innerHTML = usersBody;
+
+  const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+  // Must use array.from to create a real array from a DOM collection
+  // getElementsByClassName only returns an "array like" object
+
+  Array.from(deleteLinks, link => {
+    link.onclick = function(event) {
+      const element = event.target;
+      event.preventDefault();
+      deleteUser(element.attributes["data-id"].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    };
+  });
 });
